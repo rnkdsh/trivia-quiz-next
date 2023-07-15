2cfef845b795ab2e6a659541cfa805778c36c383
Ronak Doshi <ronakdoshi10@gmail.com>
2023-07-15 16:52:44 +0530

    create Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <nav className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 backdrop-blur-lg'>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <Image src='/trivia.svg' alt='Logo' width={100} height={24} priority />
+        <div className='flex md:order-2'></div>
+      </div>
+    </nav>
+  );
+}
