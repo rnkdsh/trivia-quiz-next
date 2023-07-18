f4cb3ff129d08181c3a39feade95ed01c95dbcda
Ronak Doshi <ronakdoshi10@gmail.com>
2023-07-18 19:37:21 +0530

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,34 @@ import { Analytics } from '@vercel/analytics/react';
 import MainContainer from './components/MainContainer';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://trivia-quiz-next.vercel.app'),
   title: 'TRIVIA',
   description:
     'Challenge your knowledge with our captivating TRIVIA quiz app and discover the joy of learning through fun and exciting questions.',
+  openGraph: {
+    title: 'TRIVIA',
+    description:
+      'Challenge your knowledge with our captivating TRIVIA quiz app and discover the joy of learning through fun and exciting questions.',
+    url: 'https://trivia-quiz-next.vercel.app',
+    siteName: 'TRIVIA',
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'TRIVIA',
+    card: 'summary_large_image',
+  },
 };
 
 export default function RootLayout({
